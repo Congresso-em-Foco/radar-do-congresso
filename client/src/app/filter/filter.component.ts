a97fe776c94ddd04f5f855e068f2a220824e7b9f
Jefferson Neves <jefferson.rpn@gmail.com>
2020-02-19 22:39:34 -0300

    Remove filtros desnecessários

--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -7,10 +7,6 @@ import { takeUntil } from 'rxjs/operators';
 
 import { estados } from '../shared/constants/estados';
 import { ParlamentarService } from '../shared/services/parlamentar.service';
-import { Tema } from '../shared/models/tema.model';
-import { TemaService } from '../shared/services/tema.service';
-import { ComissaoService } from '../shared/services/comissao.service';
-import { LiderancaService } from '../shared/services/lideranca.service';
 import { CasaService } from '../shared/services/casa.service';
 
 @Component({
@@ -25,8 +21,6 @@ export class FilterComponent implements OnInit, OnDestroy {
 
   readonly FILTRO_PADRAO_ESTADO = 'Estados';
   readonly FILTRO_PADRAO_PARTIDO = 'Partidos';
-  readonly FILTRO_PADRAO_TEMA = -1;
-  readonly FILTRO_PADRAO_TEMA_SLUG = 'todos';
 
   filtro: any;
 
@@ -36,7 +30,6 @@ export class FilterComponent implements OnInit, OnDestroy {
 
   estados: string[];
   partidosFiltradosPorEstado: string[];
-  temas: Tema[];
 
   temaSelecionado: number;
   estadoSelecionado: string;
@@ -49,22 +42,17 @@ export class FilterComponent implements OnInit, OnDestroy {
     private router: Router,
     private modalService: NgbModal,
     private parlamentarService: ParlamentarService,
-    private temaService: TemaService,
-    private comissaoService: ComissaoService,
-    private liderancaService: LiderancaService,
     private casaService: CasaService
   ) {
     this.estados = estados;
     this.estadoSelecionado = this.FILTRO_PADRAO_ESTADO;
     this.partidoSelecionado = this.FILTRO_PADRAO_PARTIDO;
-    this.temaSelecionado = this.FILTRO_PADRAO_TEMA;
 
     this.filtro = {
       nome: '',
       estado: this.estadoSelecionado,
       partido: this.partidoSelecionado,
       tema: this.temaSelecionado,
-      temaSlug: this.FILTRO_PADRAO_TEMA_SLUG,
       default: true
     };
   }
@@ -76,7 +64,6 @@ export class FilterComponent implements OnInit, OnDestroy {
       this.aplicarFiltro();
     });
     this.updateFiltroViaUrl();
-    this.getTemas();
   }
 
   open(content) {
@@ -100,12 +87,7 @@ export class FilterComponent implements OnInit, OnDestroy {
       nome: this.nomePesquisado,
       estado: this.estadoSelecionado,
       partido: this.partidoSelecionado,
-      comissao: this.comissaoSelecionada,
-      tema: this.temaSelecionado,
-      temaSlug: this.temaService.getTemaSlugById(this.temas, this.temaSelecionado),
       orientador: undefined,
-      lideranca: this.liderancaSelecionada,
-      cargoComissao: this.cargoComissaoSelecionado,
       casa: this.casaSelecionada,
       default: this.isFiltroDefault()
     };
@@ -119,11 +101,7 @@ export class FilterComponent implements OnInit, OnDestroy {
   limparFiltro() {
     this.estadoSelecionado = this.FILTRO_PADRAO_ESTADO;
     this.partidoSelecionado = this.FILTRO_PADRAO_PARTIDO;
-    this.comissaoSelecionada = this.FILTRO_PADRAO_COMISSAO_VALUE;
     this.nomePesquisado = '';
-    this.temaSelecionado = this.FILTRO_PADRAO_TEMA;
-    this.liderancaSelecionada = this.FILTRO_PADRAO_LIDERANCA;
-    this.cargoComissaoSelecionado = this.FILTRO_PADRAO_CARGO_COMISSAO;
 
     this.aplicarFiltro();
   }
@@ -139,32 +117,6 @@ export class FilterComponent implements OnInit, OnDestroy {
     this.aplicarFiltro();
   }
 
-  limparFiltroComissao() {
-    this.comissaoSelecionada = this.FILTRO_PADRAO_COMISSAO_VALUE;
-    this.aplicarFiltro();
-  }
-
-  limparFiltroTema() {
-    this.temaSelecionado = this.FILTRO_PADRAO_TEMA;
-    this.aplicarFiltro();
-  }
-
-  limparFiltroLideranca() {
-    this.liderancaSelecionada = this.FILTRO_PADRAO_LIDERANCA;
-    this.aplicarFiltro();
-  }
-
-  limparFiltroCargoComissao() {
-    this.cargoComissaoSelecionado = this.FILTRO_PADRAO_CARGO_COMISSAO;
-    this.aplicarFiltro();
-  }
-
-  getTemas() {
-    this.temaService.getTemas().pipe(takeUntil(this.unsubscribe)).subscribe((temas) => {
-      this.temas = temas;
-    });
-  }
-
   getPartidoPorEstado(casa: string) {
     this.parlamentarService
     .getPartidosPorEstado(casa)
@@ -177,40 +129,14 @@ export class FilterComponent implements OnInit, OnDestroy {
     );
   }
 
-  isTemaSelected(idTema: number) {
-    return this.temaSelecionado === idTema;
-  }
-
-  selecionaTema(idTema: number) {
-    if (this.temaSelecionado === idTema) {
-      this.temaSelecionado = this.FILTRO_PADRAO_TEMA;
-    } else {
-      this.temaSelecionado = idTema;
-    }
-    this.aplicarFiltro();
-  }
-
-  getTemaById(id: number) {
-    if (this.temas && id !== this.FILTRO_PADRAO_TEMA) {
-      return this.temas.filter(tema => tema.idTema === id)[0].tema;
-    }
-  }
-
   isFiltroDefault() {
     return ((this.filtro.nome === '' || typeof this.filtro.nome === 'undefined') &&
       this.filtro.estado === this.FILTRO_PADRAO_ESTADO &&
-      this.filtro.partido === this.FILTRO_PADRAO_PARTIDO &&
-      this.filtro.tema === this.FILTRO_PADRAO_TEMA);
+      this.filtro.partido === this.FILTRO_PADRAO_PARTIDO);
   }
 
   private updateUrlFiltro(filtro: any) {
     const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
-
-    if (filtro.tema !== this.FILTRO_PADRAO_TEMA) {
-      queryParams.tema = filtro.tema;
-    } else {
-      delete queryParams.tema;
-    }
 
     if (filtro.nome !== '' && filtro.nome !== undefined) {
       queryParams.nome = filtro.nome;
@@ -238,10 +164,6 @@ export class FilterComponent implements OnInit, OnDestroy {
       params => {
         let filtroPresente = false;
         Object.keys(params).forEach(value => {
-          if (value === 'tema') {
-            this.temaSelecionado = Number(params[value]);
-            filtroPresente = true;
-          }
           if (value === 'nome') {
             this.nomePesquisado = params[value];
             filtroPresente = true;
