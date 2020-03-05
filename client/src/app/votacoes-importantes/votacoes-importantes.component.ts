f9a977a7798ad2434ff77b1600d707e70c5cf4a1
Jefferson Neves <jefferson.rpn@gmail.com>
2020-03-05 11:37:44 -0300

    Seleciona proposição importante e exibe seus votos

--- client/src/app/votacoes-importantes/votacoes-importantes.component.ts
+++ client/src/app/votacoes-importantes/votacoes-importantes.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { ProposicaoService } from '../shared/services/proposicao.service';
+import { Proposicao } from '../shared/models/proposicao.model';
 
 @Component({
   selector: 'app-votacoes-importantes',
   templateUrl: './votacoes-importantes.component.html',
   styleUrls: ['./votacoes-importantes.component.scss']
 })
-export class VotacoesImportantesComponent implements OnInit {
+export class VotacoesImportantesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  readonly SIM = 1;
+  readonly NAO = -1;
+  readonly FALTOU = 0;
+  readonly OBSTRUCAO = 2;
+  readonly ABSTENCAO = 3;
+  readonly LIBEROU = 5;
+
+  private unsubscribe = new Subject();
+
+  public proposicoes: Proposicao[];
+  public proposicaoSelecionada: Proposicao;
+
+  p = 1;
+
+  constructor(private proposicaoService: ProposicaoService) { }
 
   ngOnInit() {
+    this.getImportantes();
+  }
+
+  getImportantes() {
+    this.proposicaoService
+      .getProposicoesImportantes()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(proposicoes => {
+        this.proposicoes = proposicoes;
+        this.proposicaoSelecionada = this.proposicoes[0];
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  selecionarProposicao(proposicao: Proposicao) {
+    this.proposicaoSelecionada = proposicao;
+  }
+
+  getTextoVoto(voto: number, votoSecreto: boolean): string {
+    let textoVoto: string;
+    if (votoSecreto) {
+      textoVoto = 'VOTO SECRETO';
+    } else {
+      switch (voto) {
+        case this.SIM:
+          textoVoto = 'SIM';
+          break;
+        case this.NAO:
+          textoVoto = 'NÃO';
+          break;
+        case this.FALTOU:
+          textoVoto = 'FALTOU';
+          break;
+        case this.OBSTRUCAO:
+          textoVoto = 'OBSTRUÇÃO';
+          break;
+        case this.LIBEROU:
+          textoVoto = 'LIBEROU';
+          break;
+        case this.ABSTENCAO:
+          textoVoto = 'ABSTENÇÃO';
+          break;
+        case undefined:
+          textoVoto = '--';
+          break;
+        default:
+          break;
+      }
+    }
+    return textoVoto;
+  }
+
+  pageChange(p: number) {
+    this.p = p;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
--- client/src/app/votacoes-importantes/votacoes-importantes.module.ts
+++ client/src/app/votacoes-importantes/votacoes-importantes.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { SharedModule } from '../shared/components/shared.module';
 import { VotacoesImportantesRoutingModule } from './votacoes-importantes-routing.module';
@@ -10,7 +13,9 @@ import { VotacoesImportantesComponent } from './votacoes-importantes.component';
   declarations: [VotacoesImportantesComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
+    NgxPaginationModule,
     VotacoesImportantesRoutingModule
   ]
 })
